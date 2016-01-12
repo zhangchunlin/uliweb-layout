fd73c5ac717c438c75bd4453abe312294878a4da
limodou <limodou@gmail.com>
2016-01-12 14:04:37 +0800

    add new QueryForm

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 uliweb-layout is used to define default layout about uliweb application. It
 include default layout and common templates.
+
+## Components
+
+* QueryForm
---
+++ uliweb_layout/query_forms.py
@@ -0,0 +1,29 @@
+from uliweb import form
+from uliweb.form.layout import QueryLayout
+
+class QueryForm(form.Form):
+    form_buttons = form.Button(value=_('Search'),
+                               _class="btn btn-primary btn-flat btn-xs search",
+                               type='submit')
+    layout_class = QueryLayout
+    form_method = 'GET'
+
+    def post_html(self):
+        buf = """
+<style>
+table.query {margin-bottom:5px;}
+table.query label {margin-left:5px;margin-right:5px;margin-bottom:0px;}
+table.query label:first-child {margin-left:0px;}
+table.query .search{margin-left:10px;}
+</style>
+<script>
+$(document).ready(function(){
+    $('#query_div').hide();
+    $('#more_query').click(function(){
+        $('#query_div').toggle();
+        $('#more_query').toggleClass("foldup")
+    });
+});
+</script>
+    """
+        return buf
