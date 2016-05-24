0713a5812959218a7807d0b671fbb2d028c8b52e
limodou <limodou@gmail.com>
2016-05-24 14:24:51 +0800

    improve layout add form

--- uliweb_layout/menu/__init__.py
+++ uliweb_layout/menu/__init__.py
@@ -207,11 +207,7 @@ def navigation(name, active='', check=None, id=None, _class=None):
     
     _navigation = settings.MENUS_CONFIG.navigation_render or default_navigation
     return import_attr(_navigation)(name=name, active=active, validators=validators, id=id, _class=_class)
-    
-def default_navigation(name, active='', validators=None, id=None, _class=None):
-    return default_menu(name, active=active,
-        validators=validators, id=id, _class=_class, menu_default_class='navigation-menu')
-    
+
 def menu(name, active='', check=None, id=None, _class=None):
     from uliweb import settings
 
@@ -280,6 +276,7 @@ def default_menu(name, active='', validators=None, id=None, _class=None,
     :param active: something like "x/y/z"
     :param check: validate callback, basic validate is defined in settings
     """
+    from uliweb.utils.common import safe_unicode
 
     s = []
     for _t, y in iter_menu(name, active, validators):
@@ -308,7 +305,7 @@ def default_menu(name, active='', validators=None, id=None, _class=None,
                     s.append('<i class="fa fa-angle-double-right"></i>')
 
 
-            s.extend([indent, '<span>', str(y['title']), '</span>'])
+            s.extend([indent, '<span>', safe_unicode(y['title']), '</span>'])
             if y['subs']:
                 s.append('<i class="fa fa-angle-left pull-right"></i>')
             s.append('</a>')
@@ -327,3 +324,69 @@ def default_menu(name, active='', validators=None, id=None, _class=None,
             s.extend([indent, '</ul>\n', indent])
 
     return ''.join(s)
+
+def default_navigation(name, active='', validators=None, id=None, _class=None,
+                 menu_default_class='nav navbar-nav'):
+    """
+    :param menu: menu item name
+    :param active: something like "x/y/z"
+    :param check: validate callback, basic validate is defined in settings
+    """
+    from uliweb.utils.common import safe_unicode
+
+    s = []
+    for _t, y in iter_menu(name, active, validators):
+        index = y['index']
+        indent = ' '*index*2
+        if _t == 'item':
+            _lica = []
+            if y['active']:
+                _lica.append('active')
+            if y['expand']:
+                _lica.append('open')
+            if y['subs']:
+                _lica.append("treeview")
+            _licstr = 'class="%s"' % (' '.join(_lica)) if _lica else ''
+            _name = ' name="%s"' % y['name']
+            s.extend([indent, '<li ', _licstr, _name, '><a href="', y['link'], '">'])
+            if 'icon' in y and y['icon']:
+                if y['icon'].startswith("ion-"):
+                    s.extend(['<i class="ion %s"></i>' % y['icon']])
+                elif y['icon'].startswith("fa-"):
+                    s.extend(['<i class="fa %s"></i>' % y['icon']])
+                else:
+                    s.extend(['<i class="fa fa-%s"></i>' % y['icon']])
+            else:
+                if index > 1:
+                    s.append('<i class="fa fa-angle-double-right"></i>')
+
+
+            s.extend([safe_unicode(y['title'])])
+            if y['subs']:
+                s.append('<i class="fa fa-angle-left pull-right"></i>')
+            s.append('</a>')
+        elif _t == 'open':
+            pass
+        elif _t == 'close':
+            s.append('</li>\n')
+        elif _t == 'begin':
+            if index == 0:
+                _id = (' id="%s"' % id) if id else ''
+                _cls = (' %s' % _class) if _class else ''
+                s.append('<ul class="%s %s"%s>\n' % (menu_default_class, _cls, _id))
+            else:
+                s.extend(['\n', indent, '<ul class="treeview-menu">\n'])
+        else:
+            s.extend([indent, '</ul>\n', indent])
+
+    return ''.join(s)
+
+def print_menu_html(name, type='side'):
+    from uliweb import settings
+
+    if type == 'side':
+        _menu = settings.MENUS_CONFIG.menu_render or default_menu
+    elif type == 'main':
+        _menu = settings.MENUS_CONFIG.menu_render or default_navigation
+    print import_attr(_menu)(name=name)
+
